74c1e5493a9ade654e34fefc48791fa1eed72099
ebeberness <enb1@uw.edu>
2023-02-16 14:45:07 -0800

    added scaling to images

--- index.js
+++ index.js
@@ -4,34 +4,34 @@ let score = 0;
 
 function preload() {
   flowerImg = loadImage('/flowergame.png');
-  beeImg = loadImage('/bee.png');
+  beeImg = loadImage('bee.png');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(100, 200, 30);
   
-  bee = createSprite(windowWidth/2, windowHeight/2);
+  
+  bee = createSprite(200,200);
   bee.addImage(beeImg);
+  bee.scale = 0.25;
   
 
   flowers = new Group();
   
   for (let i = 0; i < 10; i++) {
-    f = createSprite(random(width), random(height),
+    f = createSprite(random(windowWidth), random(windowHeight/2),
     random(25, 100), random(25, 100));
     f.addImage(flowerImg);
-    //f.shapeColor = color(random(200, 255));
+    f.scale=0.25;
     f.velocity.y = 0.5;
     flowers.add(f);
-    f.onMouseOver = removeAndScore;
+    f.onMouseOver=removeAndScore;
   } 
  
 }
 
 function draw() {
-  
-  drawSprites();
+background(100, 200, 30);
 
   bee.velocity.x = (mouseX-bee.position.x)*0.2;
   bee.velocity.y = (mouseY-bee.position.y)*0.2;
@@ -42,9 +42,11 @@ function draw() {
   else {
     text("you win!", windowWidth/2, windowHeight/2);
   } 
+
+  drawSprites();
 }
 
 function removeAndScore() {
   score += 1;
-  this.remove();
+  f.remove();
 }
