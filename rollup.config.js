d6a2e951fd1560674789596c29bfae643dfdb23f
ebeberness <enb1@uw.edu>
2023-02-20 23:36:52 -0800

    updated rollup

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ module.exports = {
   treeshake: false,
   plugins: [
     copy({
-      targets: [{ src: ["index.html", "images"], dest: "dist" }],
+      targets: [{ src: ["index.html", "images", "index.js"], dest: "dist" }],
     }),
   ]
 };
